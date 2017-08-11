29f1f19a9c11a80ae548dc0134f28d73672509c8
Anthony <non.zero@gmail.com>
2017-08-11 12:12:08 -0700

    Add model for list Item

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page
+from lists.models import Item
 
 class HomePageTest(TestCase):
         
@@ -14,4 +15,25 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+        
+class ItemModelTest(TestCase):
+    
+    def test_saving_and_retrieving_items(self):
+        # save 1st item
+        first_item = Item()
+        first_item.text = "The first (ever) item"
+        first_item.save()
+        
+        # save 2nd item
+        second_item = Item()
+        second_item.text = "Item the second"
+        second_item.save()
+        
+        # get all items and count
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        
+        # compare items equal to constants
+        self.assertEqual(saved_items[0].text, 'The first (ever) item', )
+        self.assertEqual(saved_items[1].text, 'Item the second')
         
