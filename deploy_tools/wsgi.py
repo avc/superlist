ccb573fd0584569e893dd208aecba6a13fcd4491
Anthony <anthony@percepticons.com>
2017-08-18 14:55:53 -0700

    wsgi.py uses correct path to virtualenv. Import secret_key.py.

--- deploy_tools/wsgi.py
+++ deploy_tools/wsgi.py
@@ -1,16 +1,18 @@
 import sys, os
-VIRTUALENV = "/tmp"
+VIRTUALENV = 'virtualenv'
 PROJECT = "superlists"
 
-INTERP = VIRTUALENV + "/bin/python"
+# The virtualenv directory is a sibling of this file.
+virtualenv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), VIRTUALENV)
+INTERP = virtualenv_path + "/bin/python"
 #INTERP is presented twice so that the new Python interpreter knows the actual executable path.
 if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
 
 cwd = os.getcwd()
 sys.path.append(cwd)
 sys.path.append(cwd + '/src')
-sys.path.insert(0,cwd+VIRTUALENV+'bin')
-sys.path.insert(0,cwd+VIRTUALENV+'lib/python3.6/site-packages')
+sys.path.insert(0, os.path.join(virtualenv_path, 'bin'))
+sys.path.insert(0, os.path.join(virtualenv_path, 'lib/python3.6/site-packages'))
 
 os.environ['DJANGO_SETTINGS_MODULE'] = PROJECT + ".settings"
 from django.core.wsgi import get_wsgi_application
--- superlists/settings.py
+++ superlists/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'prfdi^vbe#7xca9y-uvqcgns%bu(9xlv79=$!(9law$4y!^dns'
+from .secret_key import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
