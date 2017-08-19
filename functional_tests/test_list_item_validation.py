89d016923c6d6a13b255335bbf2c60232939c4a8
Anthony <non.zero@gmail.com>
2017-08-19 00:51:37 -0700

    Add wait_for() to enable asserting with a timeout.

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -8,26 +8,33 @@ class ItemValidationTest(FunctionalTest):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
         self.browser.get(self.live_server_url)
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys(Keys.ENTER)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
-        self.wait_for_text('cannot be blank')
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error'),
+            "You cannot have an empty list item."
+        ))
 
         # She tries again with some text for the item, which now works
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Trying again')
         inputbox.send_keys(Keys.ENTER)
+        wait_for_row_in_table('1: Trying again')
 
         # Perversely, she now decides to submit a second blank list item
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys(Keys.ENTER)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
         
         # She receives a similar warning on the list page
-        self.wait_for_text('cannot be blank')
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error'),
+            "You cannot have an empty list item."
+        ))
         
         # And she can correct it by filling some text in
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('How about now?')
         inputbox.send_keys(Keys.ENTER)
+        wait_for_row_in_table('1: Trying again')
+        wait_for_row_in_table('2: How about now?')
