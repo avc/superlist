43d5ccb01b8fce76d88576df68cd945ba6fa9d5a
Anthony <non.zero@gmail.com>
2017-08-29 16:12:02 -0700

    Don’t redirect after POST to add new item. Error on empty item.

--- lists/views.py
+++ lists/views.py
@@ -7,10 +7,15 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    data = {'list': list_}
     if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'], list=list_)
-        return redirect(f'/lists/{list_id}/')
-    return render(request, 'list.html', {'list': list_})
+        try:
+            item = Item.objects.create(text=request.POST['item_text'], list=list_)
+            item.full_clean()
+        except ValidationError:
+            item.delete()
+            data['error'] = "You can't have an empty list item."
+    return render(request, 'list.html', data)
     
 def new_list(request):
     list_ = List.objects.create()
