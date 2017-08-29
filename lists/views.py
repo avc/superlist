e37eff27346c27366e1b0b00fd887b134c377401
Anthony <non.zero@gmail.com>
2017-08-29 16:19:30 -0700

    Refactor data dictionary in view_list.

--- lists/views.py
+++ lists/views.py
@@ -7,15 +7,15 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    data = {'list': list_}
+    error = None
     if request.method == 'POST':
         try:
             item = Item.objects.create(text=request.POST['item_text'], list=list_)
             item.full_clean()
         except ValidationError:
             item.delete()
-            data['error'] = "You can't have an empty list item."
-    return render(request, 'list.html', data)
+            error = "You can't have an empty list item."
+    return render(request, 'list.html', {'list': list_, 'error': error})
     
 def new_list(request):
     list_ = List.objects.create()
